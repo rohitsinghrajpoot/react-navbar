e9a8564fee338981a656dd9779edf251b65a138f
Rohit Singh <95858652+rohitsinghrajpoot@users.noreply.github.com>
2022-04-12 04:27:19 +0530

    Add files via upload

---
+++ navbar/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  const links =["./services","./projects","./about"]
+  return (
+    <div className="App">
+    <h3 className="lg">LOGOBAKERY</h3> 
+    <h5>Services</h5>
+    <h5>Projects</h5>
+    <h5>About</h5>
+  <button className='click'>contact</button>
+  
+    </div>
+  );
+}
+
+
+export default App;
